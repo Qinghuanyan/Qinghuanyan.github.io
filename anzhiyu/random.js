5b0a942a4c1b94fc3bdd59269d974607ea16ab9a
Qinghuanyan <3185602863@qq.com>
2025-05-17 18:41:29 +0800

    Site updated: 2025-05-17 18:41:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/28/hello-world/"];function toRandomPost(){
+var posts=["2025/05/17/jisk/","2025/05/17/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
