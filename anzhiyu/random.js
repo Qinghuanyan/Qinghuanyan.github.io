b3f3cfabfce25f6ed87b24863f8ad58739be11a7
Qinghuanyan <3185602863@qq.com>
2025-06-11 15:49:05 +0800

    Site updated: 2025-06-11 15:49:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/17/道友，故事的结局重要么？/","2025/06/11/JavaNote/"];function toRandomPost(){
+var posts=["2025/06/11/JavaNote/","2025/05/17/道友，故事的结局重要么？/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
