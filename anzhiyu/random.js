2687bf73a36052b3a83522f9208ede85066cd4bf
Qinghuanyan <3185602863@qq.com>
2025-06-12 19:14:08 +0800

    Site updated: 2025-06-12 19:14:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/12/少年，孤身入局可惧否？/","2025/06/11/JavaNote/","2025/05/17/道友，故事的结局重要么？/"];function toRandomPost(){
+var posts=["2025/06/11/JavaNote/","2025/05/17/道友，故事的结局重要么？/","2025/06/12/少年，孤身入局可惧否？/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
